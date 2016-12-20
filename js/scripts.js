d692e8607b848bc6b652795aaf82038a49edaacf
Larry Tunseth <larrydeantun@gmail.com>
2016-12-20 11:00:06 -0800

    add content to buyer testimonial modal

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,7 @@ $(function() {
   $(".modal-inner").on("click", function(e) {
     e.stopPropagation();
   });
-  $("#modal-2").on("change", function() {
+  $("#buyer-modal").on("change", function() {
     if ($(this).is(":checked")) {
       $("body").addClass("modal-open");
     } else {
@@ -29,4 +29,20 @@ $(function() {
   $(".modal-inner").on("click", function(e) {
     e.stopPropagation();
   });
+  $("#seller-modal").on("change", function() {
+    if ($(this).is(":checked")) {
+      $("body").addClass("modal-open");
+    } else {
+      $("body").removeClass("modal-open");
+    }
+  });
+
+  $(".modal-fade-screen, .modal-close").on("click", function() {
+    $(".modal-state:checked").prop("checked", false).change();
+  });
+
+  $(".modal-inner").on("click", function(e) {
+    e.stopPropagation();
+  });
+
 });
