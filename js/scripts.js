5b34c045ad0166e5a53c85a4dbc39291fb6304fb
Larry Tunseth <larrydeantun@gmail.com>
2016-12-21 11:02:15 -0800

    add ablity to load more house listings

--- js/scripts.js
+++ js/scripts.js
@@ -28,3 +28,9 @@ $(document).ready(function() {
     });
   });
 });
+
+$(document).ready(function() {
+  $('.expander-trigger').click(function(){
+    $(this).toggleClass("expander-hidden");
+  });
+});
