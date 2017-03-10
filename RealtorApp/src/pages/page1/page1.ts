01090832a361725ce95f4e3356f9433b71548359
Larry Tunseth <larrydeantun@gmail.com>
2017-03-09 17:32:36 -0800

    add search page to mobile app

--- RealtorApp/src/pages/page1/page1.ts
+++ RealtorApp/src/pages/page1/page1.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+import { Page2 } from '../page2/page2';
 
 @Component({
   selector: 'page-page1',
   templateUrl: 'page1.html'
 })
 export class Page1 {
+  page2 = Page2;
+
+
 
   constructor(public navCtrl: NavController) {
-    
+
   }
 
 }
